20a863c55413e2f88563291538b3ba23490e7642
Tamirat Kebede <tamiratkebede120@gmail.com>
2023-11-24 21:00:32 +0300

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const port = 3000;
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then((data) => {
+    console.log(`connected`);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 app.listen(port, () => {
   console.log(`running @${port}`);
