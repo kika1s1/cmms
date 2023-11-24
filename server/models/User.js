4e7f996d31b2543adac0bc76fd6604b07cf856d8
Tamirat Kebede <tamiratkebede120@gmail.com>
2023-11-24 21:06:06 +0300

    create user model

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: string,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: string,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: string,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const User = mongoose.model("User", userSchema);
+
+export default User;
