6b9dc4d7e69ef13613a34223cba76c312b1e79c5
Tamirat Kebede <tamiratkebede120@gmail.com>
2023-11-25 16:02:19 +0300

    complete event page functionality

--- client/src/pages/CreateEvent.jsx
+++ client/src/pages/CreateEvent.jsx
@@ -6,15 +6,26 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
-// import { set } from "mongoose";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateEvent() {
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
   const [files, setFiles] = useState([]);
   const [formData, setFormData] = useState({
     imageUrls: [],
+    title: "",
+    description: "",
+    address: "",
+
+    date: new Date("2023-11-25T10:00:00"),
+    link: "",
   });
   const [imageUploadError, setImageUploadError] = useState(false);
   const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   console.log(formData);
   const handleImageSubmit = (e) => {
     e.preventDefault();
@@ -76,27 +87,64 @@ export default function CreateEvent() {
       imageUrls: formData.imageUrls.filter((_, i) => i !== index),
     });
   };
+
+  const handleChange = (e) => {
+    if (
+      e.target.type === "datetime-local" ||
+      e.target.type === "text" ||
+      e.target.type === "textarea"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.imageUrls.length < 1)
+        return setError("You must upload at least one image");
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/event/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      }
+      navigate(`/event/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
         Create a Event
       </h1>
-      <form className="flex flex-col sm:flex-row gap-4">
+      <form className="flex flex-col sm:flex-row gap-4" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-4 flex-1">
           <input
             type="text"
             placeholder="Title"
             className="border p-3 rounded-lg"
             id="title"
+            onChange={handleChange}
+            value={formData.title}
             maxLength="62"
             minLength="10"
-            required
-          />
-          <input
-            type="datetime-local"
-            placeholder="date time"
-            className="border p-3 rounded-lg"
-            id="date"
             required
           />
 
@@ -105,6 +153,8 @@ export default function CreateEvent() {
             placeholder="Description"
             className="border p-3 rounded-lg"
             id="description"
+            onChange={handleChange}
+            value={formData.description}
             required
           />
 
@@ -114,6 +164,8 @@ export default function CreateEvent() {
             className="border p-3 rounded-lg"
             id="address"
             required
+            onChange={handleChange}
+            value={formData.address}
           />
           <input
             type="datetime-local"
@@ -179,9 +231,13 @@ export default function CreateEvent() {
                 </button>
               </div>
             ))}
-          <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-            Create Event
+          <button
+            disabled={loading || uploading}
+            className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          >
+            {loading ? "Creating..." : "Create Event"}
           </button>
+          {error && <p className="text-red-700 text-sm">{error}</p>}
         </div>
       </form>
     </main>
