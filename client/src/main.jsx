5d57ce8abaa976609a3fbbaec974bf0c73b75477
Tamirat Kebede <tamiratkebede120@gmail.com>
2023-11-25 09:17:13 +0300

    add redux persit to handle login user on local storage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
