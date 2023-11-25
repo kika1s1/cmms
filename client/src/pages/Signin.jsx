6964d8fc5cf5f8de4fb149f5179fe108e06296fd
Tamirat Kebede <tamiratkebede120@gmail.com>
2023-11-25 08:37:06 +0300

    complete sign in functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -18,7 +18,7 @@ const Signin = () => {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch("/api/auth/signup", {
+      const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -35,7 +35,7 @@ const Signin = () => {
       }
       setLoading(false);
       setError(null);
-      navigate("/signin");
+      navigate("/");
     } catch (error) {
       setError(error.message);
       setLoading(false);
@@ -43,15 +43,8 @@ const Signin = () => {
   };
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          onChange={handleChangle}
-          id="username"
-          placeholder="username..."
-          className="border p-3 rounded-lg focus:outline-none"
-        />
         <input
           type="email"
           onChange={handleChangle}
@@ -74,9 +67,9 @@ const Signin = () => {
         </button>
       </form>
       <div className="flex gap-2 mt-5">
-        <p>have an account?</p>
-        <Link to="/signin">
-          <span className="text-blue-700">Sign in</span>
+        <p>Dont have an account?</p>
+        <Link to="/signup">
+          <span className="text-blue-700">Sign out</span>
         </Link>
       </div>
       {error && <p className="text-center text-red-500">{error}</p>}
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -22,24 +22,18 @@ const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) {
-      return next(errorHandler(404, "user does not found"));
-    }
-
-    const isMatched = await bcrypt.compare(password, validUser.password);
-    if (!isMatched) {
-      return next(errorHandler(401, "invalid credentials"));
-    }
-    const token = await jwt.sign({ id: validUser._id }, process.env.SECRET);
-    const { password: pass, ...res } = validUser._doc;
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
     res
-      .cookie("access-token", token, { httpOnly: true })
+      .cookie("access_token", token, { httpOnly: true })
       .status(200)
-      .json({ rest });
-
-    // res.json({})
+      .json(rest);
   } catch (error) {
     next(error);
   }
 };
+
 export { signup, signin };
