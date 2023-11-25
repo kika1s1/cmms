58443d8a668a1579ee3258e91cc3456384b6bd97
Tamirat Kebede <tamiratkebede120@gmail.com>
2023-11-25 11:59:57 +0300

    add delete user functionality

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { google, signin, signup } from "../controllers/auth.js";
+
 const router = Router();
 router.post("/signup", signup);
 router.post("/signin", signin);
