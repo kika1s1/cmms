926b1bb638780aac1dc1b6c1f44d12c5f4b09b98
Tamirat Kebede <tamiratkebede120@gmail.com>
2023-11-25 12:14:32 +0300

    add sign out functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,29 +1,26 @@
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import bcryptjs from "bcryptjs";
 import errorHandler from "../utils/error.js";
-const signup = async (req, res, next) => {
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = new User({
-    username,
-    password: hashedPassword,
-    email,
-  });
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
-    await newUser.save().then(() => {
-      res.json({ message: "user created successfully" });
-    });
+    await newUser.save();
+    res.status(201).json("User created successfully!");
   } catch (error) {
     next(error);
   }
 };
-const signin = async (req, res, next) => {
+
+export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "User not found!"));
-    const validPassword = bcrypt.compareSync(password, validUser.password);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
@@ -35,7 +32,8 @@ const signin = async (req, res, next) => {
     next(error);
   }
 };
-const google = async (req, res, next) => {
+
+export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     if (user) {
@@ -71,4 +69,11 @@ const google = async (req, res, next) => {
   }
 };
 
-export { signup, signin, google };
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("User has been logged out!");
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,11 @@
-import { Router } from "express";
-import { google, signin, signup } from "../controllers/auth.js";
+import express from "express";
+import { google, signOut, signin, signup } from "../controllers/auth.js";
 
-const router = Router();
+const router = express.Router();
+
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/google", google);
+router.get("/signout", signOut);
 
 export default router;
